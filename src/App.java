a916a83e5f20c670778b9c14cffe469ebc7ee2da
David-hub0 <david-schemmann@web.de>
2020-12-14 16:22:04 +0100

    Kleine Änderungen Tests^^

--- src/App.java
+++ src/App.java
@@ -5,11 +5,11 @@ public class App {
 	
 	/*
 	v5.0 complete Overhaul
-	v5.1 implemented Multiplayer - requires seperate server
+	v5.1 implemented Multiplayer - requires separate server
 	v5.2 cleaning up MP changes
 	v5.3 fixing lift&drop to be single UTF command
 	v5.4 more fixes, adding save/load
-	v5.5 fixing screen size issuses
+	v5.5 fixing screen size issues
 	v5.6 adding cloud support							[https://sites.google.com/d/1GsLNXg9rv7_9r5kw578GObw9vDfiHWGt/p/1prhGM0SaKcN-seMWH2FTXSzfM8RRKtWW/edit	OR	domassets]
 	v5.7 fixing cloud, adding motionListener/timer
 	v5.8 changing to sendBytes
@@ -21,7 +21,7 @@ public class App {
 		
 		if(JOptionPane.showConfirmDialog(null, "Are you the host?") == 0) {
 			new GameServer();
-			System.out.println("Starting Server");
+			System.out.println("Starting Server /n  Ya Gat Hugged");
 		}
 		else {
 			Toolkit tk = Toolkit.getDefaultToolkit();
